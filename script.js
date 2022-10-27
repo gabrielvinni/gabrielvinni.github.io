a8726359b2c41d1715fc735b544985a104753f2d
Gabriel Vinicius dos Santos <100848560+gabrielvinni@users.noreply.github.com>
2022-10-27 16:39:01 -0300

    Interação em "Sobre mim" feito

---
+++ script.js
@@ -0,0 +1,35 @@
+function txt1() {
+    if (phtml.style.display == 'block') {
+        phtml.style.display = 'none'
+        b1.style.animation = 'girav 400ms forwards'
+        esp1.style.height = '60px'
+    }else {
+        phtml.style.display = 'block'
+        b1.style.animation = 'gira 400ms forwards'
+        esp1.style.height = '120px'
+    }
+}
+
+function txt2() {
+    if (pcss.style.display == 'block') {
+        pcss.style.display = 'none'
+        b2.style.animation = 'girav 400ms forwards'
+        esp2.style.height = '60px'
+    }else {
+        pcss.style.display = 'block'
+        b2.style.animation = 'gira 400ms forwards'
+        esp2.style.height = '100px'
+    }
+}
+
+function txt3() {
+    if (pjs.style.display == 'block') {
+        pjs.style.display = 'none'
+        b3.style.animation = 'girav 400ms forwards'
+        esp3.style.height = '60px'
+    }else {
+        pjs.style.display = 'block'
+        b3.style.animation = 'gira 400ms forwards'
+        esp3.style.height = '120px'
+    }
+}
