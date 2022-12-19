928174f6906b786c833167b5c0431500ba02008f
Gabriel Vinicius dos Santos <gabrielvini150@gmail.com>
2022-12-19 12:00:11 -0300

    alteração no botão menu

--- script.js
+++ script.js
@@ -1,75 +1,80 @@
+var mn = 0
+var ht = 0
+var cs = 0
+var j = 0
+
 function txt1() {
-    if (phtml.style.display == 'block') {
-        phtml.style.display = 'none'
-        b1.style.animation = 'girav 400ms forwards'
+    if (ht == 1) {
+        ht = 0
+        b1.style.animation = 'girav .4s forwards'
         esp1.style.height = '60px'
     }else {
-        phtml.style.display = 'block'
-        b1.style.animation = 'gira 400ms forwards'
+        ht = 1
+        b1.style.animation = 'gira .4s forwards'
         esp1.style.height = '120px'
         
-        pcss.style.display = 'none'
-        b2.style.animation = 'girav 400ms forwards'
+        cs = 0
+        b2.style.animation = 'girav .4s forwards'
         esp2.style.height = '60px'
 
-        pjs.style.display = 'none'
-        b3.style.animation = 'girav 400ms forwards'
+        j = 0
+        b3.style.animation = 'girav .4s forwards'
         esp3.style.height = '60px'
     }
 }
 
 function txt2() {
-    if (pcss.style.display == 'block') {
-        pcss.style.display = 'none'
-        b2.style.animation = 'girav 400ms forwards'
+    if (cs == 1) {
+        cs = 0
+        b2.style.animation = 'girav .4s forwards'
         esp2.style.height = '60px'
     }else {
-        pcss.style.display = 'block'
-        b2.style.animation = 'gira 400ms forwards'
+        cs = 1
+        b2.style.animation = 'gira .4s forwards'
         esp2.style.height = '100px'
 
-        phtml.style.display = 'none'
-        b1.style.animation = 'girav 400ms forwards'
+        ht = 0
+        b1.style.animation = 'girav .4s forwards'
         esp1.style.height = '60px'
 
-        pjs.style.display = 'none'
-        b3.style.animation = 'girav 400ms forwards'
+        j = 0
+        b3.style.animation = 'girav .4s forwards'
         esp3.style.height = '60px'
     }
 }
 
 function txt3() {
-    if (pjs.style.display == 'block') {
-        pjs.style.display = 'none'
-        b3.style.animation = 'girav 400ms forwards'
+    if (j == 1) {
+        j = 0
+        b3.style.animation = 'girav .4s forwards'
         esp3.style.height = '60px'
     }else {
-        pjs.style.display = 'block'
-        b3.style.animation = 'gira 400ms forwards'
+        j = 1
+        b3.style.animation = 'gira .4s forwards'
         esp3.style.height = '120px'
 
-        phtml.style.display = 'none'
-        b1.style.animation = 'girav 400ms forwards'
+        ht = 0
+        b1.style.animation = 'girav .4s forwards'
         esp1.style.height = '60px'
 
-        pcss.style.display = 'none'
-        b2.style.animation = 'girav 400ms forwards'
+        cs = 0
+        b2.style.animation = 'girav .4s forwards'
         esp2.style.height = '60px'
     }
 }
 
 function menu() {
-    if(mn.style.display == 'block') {
-        mn.style.display = 'none'
-        barra1.style.animation = 'br1v .7s forwards'
-        barra2.style.animation = 'br2v .7s forwards'
-        barra3.style.animation = 'br3v .7s forwards'
+    if(mn == 1) {
+        mn = 0
+        barra1.style.animation = 'br1v .4s forwards'
+        barra2.style.animation = 'br2v .4s forwards'
+        barra3.style.animation = 'br3v .4s forwards'
         topo.style.height = '53px'
     }else {
-        mn.style.display = 'block'
-        barra1.style.animation = 'br1 .7s forwards'
-        barra2.style.animation = 'br2 .7s forwards'
-        barra3.style.animation = 'br3 .7s forwards'
-        topo.style.height = '205px'
+        mn = 1
+        barra1.style.animation = 'br1 .4s forwards'
+        barra2.style.animation = 'br2 .4s forwards'
+        barra3.style.animation = 'br3 .4s forwards'
+        topo.style.height = '208px'
     }
 }
