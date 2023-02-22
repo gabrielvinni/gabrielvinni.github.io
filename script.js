458556f4f76c0fe3aa023c565af4ce0d688eedca
Gabriel Vinicius dos Santos <100848560+gabrielvinni@users.noreply.github.com>
2023-02-22 16:12:06 -0300

    redesing do projeto

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 var mn = 0
 
-
 function mudou_tamanho() {
     if (window.innerWidth >= 481){
         topo.style.height = '53px'
@@ -23,6 +22,6 @@ function menu() {
         barra1.style.animation = 'br1 .4s forwards'
         barra2.style.animation = 'br2 .4s forwards'
         barra3.style.animation = 'br3 .4s forwards'
-        topo.style.height = '215px'
+        topo.style.height = '225px'
     }
 }
