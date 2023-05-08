52d98e89dba9d195245c303e00af550db2181f14
Gabriel Vinicius dos Santos <100848560+gabrielvinni@users.noreply.github.com>
2023-05-08 16:18:22 -0300

    mudança no menu

---
+++ teste-script.js
@@ -0,0 +1,25 @@
+function BTmenu() {
+    const botao = document.querySelector("#botao")
+    const menu = document.querySelector("#menu")
+    const topo = document.querySelector('#topo')
+    botao.classList.toggle("botao-aberto")
+    menu.classList.toggle("menu-aberto")
+    topo.classList.toggle('topo-preso')
+    menu.style.transition = '.5s'
+}
+
+function BTmenuSome() {
+    const botao = document.querySelector("#botao")
+    const menu = document.querySelector("#menu")
+    const topo = document.querySelector('#topo')
+    if (window.innerWidth <= 820) {
+        botao.classList.toggle('botao-aberto')
+        menu.classList.toggle('menu-aberto') 
+        topo.classList.toggle('topo-preso')
+        menu.style.transition = 'none'
+        console.log("ta aqui")
+    } 
+    else if (topo.classList = 'topo-preso') {
+        topo.classList.toggle('topo-preso')
+    }
+}
