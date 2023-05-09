db30bccc86b4ef75e81cbacdaf69712e21adeb8e
Gabriel Vinicius dos Santos <100848560+gabrielvinni@users.noreply.github.com>
2023-05-09 16:36:11 -0300

    Ajuste no JS

--- teste-script.js
+++ teste-script.js
@@ -1,25 +1,24 @@
 function BTmenu() {
-    const botao = document.querySelector("#botao")
-    const menu = document.querySelector("#menu")
-    const topo = document.querySelector('#topo')
-    botao.classList.toggle("botao-aberto")
-    menu.classList.toggle("menu-aberto")
-    topo.classList.toggle('topo-preso')
-    menu.style.transition = '.5s'
+    const botao = document.querySelector("#botao");
+    const menu = document.querySelector("#menu");
+    const topo = document.querySelector('#topo');
+    botao.classList.toggle("botao-aberto");
+    menu.classList.toggle("menu-aberto");
+    topo.classList.toggle('topo-preso');
+    menu.style.transition = '.5s';
 }
 
 function BTmenuSome() {
-    const botao = document.querySelector("#botao")
-    const menu = document.querySelector("#menu")
-    const topo = document.querySelector('#topo')
+    const botao = document.querySelector("#botao");
+    const menu = document.querySelector("#menu");
+    const topo = document.querySelector('#topo');
     if (window.innerWidth <= 820) {
-        botao.classList.toggle('botao-aberto')
-        menu.classList.toggle('menu-aberto') 
-        topo.classList.toggle('topo-preso')
-        menu.style.transition = 'none'
-        console.log("ta aqui")
+        botao.classList.toggle('botao-aberto');
+        menu.classList.toggle('menu-aberto');
+        topo.classList.toggle('topo-preso');
+        menu.style.transition = 'none';
     } 
     else if (topo.classList = 'topo-preso') {
-        topo.classList.toggle('topo-preso')
+        topo.classList.toggle('topo-preso');
     }
 }
