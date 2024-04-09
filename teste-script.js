edc156d6be1fac6f68fc711d5cbc3a08b33f06ba
Gabriel Vinicius dos Santos <100848560+gabrielvinni@users.noreply.github.com>
2024-04-09 10:23:28 -0300

    Update teste-script.js

--- teste-script.js
+++ teste-script.js
@@ -1,8 +1,8 @@
-const btnMobile = document.querySelector("#btn-mobile");
-btnMobile.addEventListener('click', abrirMenu);
+const btnMobile = document.querySelector("#btn-mobile")
+btnMobile.addEventListener('click', abrirMenu)
 
 function abrirMenu() {
-    const nav = document.querySelector("#nav");
+    const nav = document.querySelector("#nav")
     btnMobile.classList.toggle('btn-animation')
     nav.classList.toggle('ativo')
 }
